eca4c2379b7b4aa2515cb63a509257c7deff49b1
FELIPE VASCONCELOS MARTINS <102231@pmenos.com.br>
2019-04-02 15:26:18 -0300

    Projeto em dotnet core 2.2 e front em react

---
+++ QUAY.PracticalTest/QUAY.ServiceAPI/Controllers/CPTECApiController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QUAY.Models;
+using RestSharp;
+
+namespace QUAY.ServiceAPI.Controllers
+{
+    [Route("api/CPTEC")]
+    [ApiController]
+    public class CPTECApiController : ControllerBase
+    {
+
+        //[Route("GetAll")]
+        //public List<Teste> Get()
+        //{
+        //    //MOCK   
+        //    return new List<Teste>() {
+        //        new Teste {Id = 123 ,CidadeT = new Teste.CidadeTeste{Nome = "Teresina" } },
+        //    };
+        //}
+
+        [HttpGet, Route("GetByCityId/{cityId}")]
+        public Cidade GetCity(int cityId)
+        {
+            var client = new RestClient($"http://servicos.cptec.inpe.br/XML/cidade/7dias/{cityId}/previsao.xml");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+            var request = new RestRequest();
+            request.XmlSerializer = new RestSharp.Serializers.DotNetXmlSerializer();
+            request.RequestFormat = DataFormat.Xml;
+
+
+            // execute the request
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+
+            var strReader = new StringReader(content);
+            var serializer = new XmlSerializer(typeof(Cidade));
+            var xmlReader = new XmlTextReader(strReader);
+            var cidade = (Cidade)serializer.Deserialize(xmlReader);
+
+
+
+            cidade.Previsao.ForEach(x => x.Tempo = Siglas.siglas[x.Tempo]);
+            return cidade;
+        }
+
+        [HttpGet, Route("GetByCityName/{nomeCidade}")]
+        public List<Cidade> GetCities(string nomeCidade)
+        {
+            var client = new RestClient($"http://servicos.cptec.inpe.br/XML/listaCidades?city={nomeCidade}");
+            var request = new RestRequest()
+            {
+                XmlSerializer = new RestSharp.Serializers.DotNetXmlSerializer(),
+                RequestFormat = DataFormat.Xml
+            };
+
+            // execute the request
+            var response = client.Execute<Cidades>(request);
+            var content = response.Data; // raw content as string
+
+            return content.cidades;
+        }
+
+    }
+
+    //public class TesteApiController : ControllerBase
+    //{
+    //    [HttpPost]
+    //    public List<Teste> Get(string cityId)
+    //    {
+    //        //MOCK   
+    //        return new List<Teste>() {
+    //            new Teste {Id = 123 ,CidadeT = new Teste.CidadeTeste{Nome = "Teresina" } },
+    //        };
+    //    }
+    //}
+    //public class Teste
+    //{
+    //    public int Id { get; set; }
+    //    public CidadeTeste CidadeT { get; set; }
+    //    public class CidadeTeste
+    //    {
+    //        public string Nome { get; set; }
+    //    }
+    //}
+
+    public class CidadeViewModel
+    {
+        public string cityId { get; set; }
+    }
+
+
+}
---
+++ QUAY.PracticalTest/QUAY.ServiceAPI/Startup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace QUAY.ServiceAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(
+                options => options.AddPolicy(
+                    "AllowAll", p =>
+                    {
+                        p.AllowAnyOrigin();
+                        p.AllowAnyMethod();
+                        p.AllowAnyHeader();
+                    }));
+
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAll"));
+            });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Tecnical Test Quay HTTP API",
+                    Version = "v1",
+                    Description = "Tecnical Test Quay HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+
+                // Defina o caminho dos comentários para o Swagger.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors("AllowAll");
+
+            var pathBase = Configuration["PATH_BASE"];
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseSwagger()
+               .UseSwaggerUI(c =>
+               {
+                   c.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "Tecnical Test Quay V1");
+                   c.OAuthClientId("tecnicaltestquayui");
+                   c.OAuthAppName("Tecnical Test Quay UI");
+               });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
